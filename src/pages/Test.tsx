74441f5dd18483c73ab209b2b8a5a4feb84811f2
subhinkrishna <subhinkrishna.suresh@gmail.com>
2026-09-19 14:42:13 +0530

    svg implementation ref

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,7 +2,7 @@ import rightLeafSvg from '../assets/svgs/Right leaf flag.svg'
 
 export default function Test() {
   return (
-    <div className="flex items-center h-screen justify-center">
+    <div className="flex  items-center h-screen justify-center">
       <img src={rightLeafSvg} alt="Right leaf flag" />
     </div>
   )
